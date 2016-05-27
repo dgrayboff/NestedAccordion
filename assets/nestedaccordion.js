30a7f6a6058e70382513c7839a9b95e144d76c79
David <dgrayboff@gmail.com>
2016-05-27 13:58:44 -0400

    JS changes

---
+++ assets/nestedaccordion.js
@@ -0,0 +1,6 @@
+var frm = document.querySelector('.header');
+var txt = document.querySelector('.text_section');
+
+frm.addEventListener('click', function () {
+	txt.classList.remove("text_section");
+});
